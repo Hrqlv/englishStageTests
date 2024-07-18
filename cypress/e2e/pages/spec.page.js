cb51d8b88a514b9bee821cb6e648a62e815386ac
Hrqlv <henriquelopesvelozo@gmail.com>
2024-07-18 16:20:53 -0300

    Ajustes nas validaçoes tanto na spec e na page

--- cypress/e2e/pages/spec.page.js
+++ cypress/e2e/pages/spec.page.js
@@ -7,8 +7,11 @@ class CadatroPage {
 
     }
 
-    realizarCadastroDadosValidos() {
+    clicarEmFazerInscricao() {
         cy.get('[data-cy="button-btn-enroll"]').click()
+    }
+
+    realizarCadastroDadosValidos() {
         cy.get('input[id="signup-personal-data-firstName"]').type(userData.nome)
         cy.get('input[id="signup-personal-data-lastName"]').type(userData.sobrenome)
         cy.get('input[id="signup-personal-data-birthDate"]').type(userData.dataDeNasc)
@@ -18,6 +21,63 @@ class CadatroPage {
         cy.get('input[id="signup-personal-data-password"]').type(userData.senha)
         cy.get('input[id="signup-personal-data-password-confirm"]').type(userData.confirmarSenha)
         cy.get('button[id="signup_submit_button_1"]').click()
+    }
+
+    // realizarCadastroDadosEmBranco() {
+    //     cy.get('input[id="signup-personal-data-firstName"]',).type()
+    //     cy.get('input[id="signup-personal-data-lastName"]',).type()
+    //     cy.get('input[id="signup-personal-data-birthDate"]',).type()
+    //     cy.get('input[id="signup-personal-data-cpf"]',).type()
+    //     cy.get('input[id="signup-personal-data-email"]',).type()
+    //     cy.get('input[id="signup-personal-data-email-confirm"]',).type()
+    //     cy.get('input[id="signup-personal-data-password"]',).type()
+    //     cy.get('input[id="signup-personal-data-password-confirm"]',).type()
+    //     cy.get('button[id="signup_submit_button_1"]').click()
+    // }
+
+
+    realizarValidacaoDeEmailInvalido() {
+        cy.get('input[id="signup-personal-data-firstName"]').type(userData.nome)
+        cy.get('input[id="signup-personal-data-lastName"]').type(userData.sobrenome)
+        cy.get('input[id="signup-personal-data-birthDate"]').type(userData.dataDeNasc)
+        cy.get('input[id="signup-personal-data-cpf"]').type(userData.cpf)
+        cy.get('input[id="signup-personal-data-email"]').type('email-invalido')
+        cy.get('input[id="signup-personal-data-email-confirm"]').type('email-invalido')
+        cy.get('input[id="signup-personal-data-password"]').type(userData.senha)
+        cy.get('input[id="signup-personal-data-password-confirm"]').type(userData.confirmarSenha)
+    }
+
+    realizarValidacaoDeSenhasNaoCoincidem() {
+        cy.get('input[id="signup-personal-data-firstName"]').type(userData.nome)
+        cy.get('input[id="signup-personal-data-lastName"]').type(userData.sobrenome)
+        cy.get('input[id="signup-personal-data-birthDate"]').type(userData.dataDeNasc)
+        cy.get('input[id="signup-personal-data-cpf"]').type(userData.cpf)
+        cy.get('input[id="signup-personal-data-email"]').type(userData.email)
+        cy.get('input[id="signup-personal-data-email-confirm"]').type(userData.confirmarEmail)
+        cy.get('input[id="signup-personal-data-password"]').type(userData.senha)
+        cy.get('input[id="signup-personal-data-password-confirm"]').type('senha123')
+    }
+
+    realizarValidacaoDeCPFInvalido() {
+        cy.get('input[id="signup-personal-data-firstName"]').type(userData.nome)
+        cy.get('input[id="signup-personal-data-lastName"]').type(userData.sobrenome)
+        cy.get('input[id="signup-personal-data-birthDate"]').type(userData.dataDeNasc)
+        cy.get('input[id="signup-personal-data-cpf"]').type('123456')
+        cy.get('input[id="signup-personal-data-email"]').type(userData.email)
+        cy.get('input[id="signup-personal-data-email-confirm"]').type(userData.confirmarEmail)
+        cy.get('input[id="signup-personal-data-password"]').type(userData.senha)
+        cy.get('input[id="signup-personal-data-password-confirm"]').type(userData.confirmarSenha)
+    }
+
+    realizarValidacaoDeDataNascInvalida() {
+        cy.get('input[id="signup-personal-data-firstName"]').type(userData.nome)
+        cy.get('input[id="signup-personal-data-lastName"]').type(userData.sobrenome)
+        cy.get('input[id="signup-personal-data-birthDate"]').type('2607200')
+        cy.get('input[id="signup-personal-data-cpf"]').type(userData.cpf)
+        cy.get('input[id="signup-personal-data-email"]').type(userData.email)
+        cy.get('input[id="signup-personal-data-email-confirm"]').type(userData.confirmarEmail)
+        cy.get('input[id="signup-personal-data-password"]').type(userData.senha)
+        cy.get('input[id="signup-personal-data-password-confirm"]').type(userData.confirmarSenha)
     }
   }
   
--- cypress/e2e/specs/spec.cy.js
+++ cypress/e2e/specs/spec.cy.js
@@ -6,30 +6,42 @@ describe('Tests Cadastro English Stage - QA Challenge', () => {
 
   beforeEach(() => {
     cy.visit('https://qastage.buildbox.one/18/cadastro/')
+    CadastroPage.clicarEmFazerInscricao()
   })
 
   it('Cadastro com dados válidos', () => {
-   //Todos os campos preenchidos corretamente.
+   //Todos os campos preenchidos corretamente
     CadastroPage.realizarCadastroDadosValidos()
   })
 
-  it('Campos Obrigatórios em Branco', () => {
-
-  })
+  // it('Campos Obrigatórios em Branco', () => {
+    //Deixar cada campo obrigatório em branco um por vez e tentar submeter o formulário
+  //   CadastroPage.realizarCadastroDadosEmBranco()
+  // })
 
   it('Formato de Email Inválido', () => {
-
+    //Inserir um email com formato inválido
+    CadastroPage.realizarValidacaoDeEmailInvalido()
+    cy.contains('Email inválido.').should('be.visible');
+    cy.contains('Precisa ser email').should('be.visible');
+   
   })
 
   it('Senhas Não Coincidem', () => {
-
+    //Inserir senhas diferentes nos campos de senha e confirmação de senha
+    CadastroPage.realizarValidacaoDeSenhasNaoCoincidem()
+    cy.contains('As senhas não são iguais.',).should('be.visible');
   })
 
   it('CPF Inválido', () => {
-
+    //Inserir um CPF com formato inválido
+    CadastroPage.realizarValidacaoDeCPFInvalido()
+    cy.contains('CPF inválido.',).should('be.visible');
   })
 
   it('Data de Nascimento Inválida', () => {
-
+    //Inserir uma data de nascimento no futuro ou em um formato inválido
+    CadastroPage.realizarValidacaoDeDataNascInvalida()
+    cy.contains('Data de nascimento inválida.',).should('be.visible');
   })
 })
